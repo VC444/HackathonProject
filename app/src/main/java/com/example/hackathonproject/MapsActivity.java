ceadd45a9100c0fbdb5bc5854a689cf64ce6d90f
Vignesh Chandrasekharan <vchan444@gmail.com>
2017-03-04 13:40:11 -0600

    Just checking if Github is working

--- app/src/main/java/com/example/hackathonproject/MapsActivity.java
+++ app/src/main/java/com/example/hackathonproject/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.example.hackathonproject;
+package com.example.hackathonproject;   // If you see this then Github is working 
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
