4658338e970f99f177106a8a6c4290e8caeca824
Akshay <axr151030@utdallas.edu>
2017-03-04 13:46:31 -0600

    added comment on top!

--- app/src/main/java/com/example/hackathonproject/MapsActivity.java
+++ app/src/main/java/com/example/hackathonproject/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.example.hackathonproject;   // If you see this then Github is working!
+package com.example.hackathonproject; //added a comment
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
